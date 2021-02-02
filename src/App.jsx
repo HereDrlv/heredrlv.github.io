115511832f0eda2b35dc523fb1f84b5aa74a81cb
HereDrlv <549414560@qq.com>
2021-02-02 17:37:34 +0800

    add style for Home

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,15 @@ import { Home } from "./components/Home/Home.jsx";
 import { Blog } from "./components/Blog/Blog.jsx";
 import { AboutMe } from "./components/AboutMe/AboutMe.jsx";
 
+import "./App.less";
+
 export class App extends React.Component {
     render() {
         return (
         <Router>
             {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. 
-                也就是说和顺序有关，这不是完全匹配*/}
+                也就是说和顺序有关，这不是完全匹配 */}
             <Switch>
                 <Route path="/blog">
                     <Blog />
